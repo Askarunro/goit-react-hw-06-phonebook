6c063dd251cf94a54247b96707ce45ed82437fff
Askarunro <paskar.y.a@gmail.com>
2022-06-01 19:49:25 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,47 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createAction, createReducer, createSlice} from "@reduxjs/toolkit";
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
-const increment = createAction('myValue')
+// export const add = createAction("items/add");
+// export const remove = createAction("items/remove");
+export const searchContact = createAction("filter/searchContact");
+
+const itemsSlice = createSlice({
+  name: "items",
+  initialState: [],
+  reducers: {
+    add(state, action) {
+      return [...state, action.payload];
+    },
+    remove(state, action) {
+      return state.filter((item) => item.name !== action.payload);
+    },
+  },
+});
+
+export const {add,remove}=itemsSlice.actions
+
+const filterReducer = createReducer("", {
+  [searchContact]: (state, action) => (state = action.payload),
+});
+
+export const store = configureStore({
+  reducer: {
+    items: itemsSlice.reducer,
+    filter: filterReducer,
+  },
+});
+
+
+
+// const itemsReducer = createReducer([], {
+//   [add]: (state, action) => [...state, action.payload],
+//   [remove]: (state, action) =>
+//     state.filter((item) => item.name !== action.payload),
+// });
+
+// export const store = configureStore({
+//   reducer: {
+//     items: itemsReducer,
+//     filter: filterReducer,
+//   },
+// });
